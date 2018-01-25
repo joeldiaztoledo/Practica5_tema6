7dba955f04a484fe0700895eff5a0f8cdbd6cdd0
joeldiaz <joeldiaztoledo@gmail.com>
2018-01-25 20:16:10 +0000

    Modificacion opcional

--- src/practicatema6.java
+++ src/practicatema6.java
@@ -12,9 +12,10 @@ public class practicatema6 {
 		while (opcion != 0) {
 			System.out.println("Menú.");
 			System.out.println("1.- Imprimir el mayor valor del array.");
-			System.out.println("2.- Imprimir el menor valor de larray.");
+			System.out.println("2.- Imprimir el menor valor del array.");
 			System.out.println("3.- Calcular la media de todos los valores del array.");
 			System.out.println("4.- Imprimir todos los valores del array.");
+			System.out.println("5.- Introducir un numero y ver cuantas veces se repite.");
 			System.out.println("0.- Salir del programa.");
 			System.out.print("Elija una opción: ");
 			opcion = sc.nextInt();
@@ -30,6 +31,11 @@ public class practicatema6 {
 						break;
 			case 4 : 	
 						lectura(array);
+						break;
+			case 5 : 	
+						System.out.println("Introduzca el número que desea buscar: ");
+						int numero = sc.nextInt();
+						System.out.println("Las veces que se repite el numero es: " + buscar(array, numero) + ".");
 						break;
 			case 0: break;
 			default:    System.out.println("Introduzca una opción válida.");
@@ -79,6 +85,17 @@ public class practicatema6 {
 			System.out.print(array[i] + " ");
 		}
 		System.out.println("}");
+	}
+	
+	public static int buscar (int[] array, int numero) {
+		int veces = 0;
+		int i = 0;
+		for (i=0; i<20; i++) {
+			if (numero == array[i]) {
+				veces++;
+			}
+		}
+		return veces;
 	}
 	
 }
