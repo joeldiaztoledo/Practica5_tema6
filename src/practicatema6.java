fa36fd60d73f6a5d5db18bbab9e488633e62fdd7
Joel Díaz Toledo <34549644+joeldiaztoledo@users.noreply.github.com>
2018-02-15 18:11:24 +0000

    Creamos atributos globales.
    En vez de usar variables locales, creo unos atributos globales que sean usados cuando sea necesario.

--- src/practicatema6.java
+++ src/practicatema6.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 public class practicatema6 {
-
+	
+	public static int [] array = new int[20];
+	public static int opcion = 1;
+	public static int max = 0;
+	public static int min = 10;
+	public static double avg = 0;
+	public static int veces = 0;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int [] array = new int[20];
 		int i = 0;
-		int opcion = 1;
 		for(i=0; i<20; i++) {
 			array[i]= (int) ((Math.random()*(10))+1);
 		}
 		while (opcion != 0) {
-			System.out.println("Menú.");
+			System.out.println("MenÃº.");
 			System.out.println("1.- Imprimir el mayor valor del array.");
 			System.out.println("2.- Imprimir el menor valor del array.");
 			System.out.println("3.- Calcular la media de todos los valores del array.");
 			System.out.println("4.- Imprimir todos los valores del array.");
 			System.out.println("5.- Introducir un numero y ver cuantas veces se repite.");
 			System.out.println("0.- Salir del programa.");
-			System.out.print("Elija una opción: ");
+			System.out.print("Elija una opciÃ³n: ");
 			opcion = sc.nextInt();
 			switch (opcion) {
 			case 1 : 	
 						System.out.println("El valor mas grande es: " + mayor(array) + ".");
 						break;
 			case 2 :  	
-						System.out.println("El valor mas pequeño es: " + menor(array) + ".");
+						System.out.println("El valor mas pequeÃ±o es: " + menor(array) + ".");
 						break;
 			case 3 :  	
 						System.out.println("La media de los valores del array es: " + media(array) + ".");
@@ -33,12 +38,12 @@ public class practicatema6 {
 						lectura(array);
 						break;
 			case 5 : 	
-						System.out.println("Introduzca el número que desea buscar: ");
+						System.out.println("Introduzca el nÃºmero que desea buscar: ");
 						int numero = sc.nextInt();
 						System.out.println("Las veces que se repite el numero es: " + buscar(array, numero) + ".");
 						break;
 			case 0: break;
-			default:    System.out.println("Introduzca una opción válida.");
+			default:    System.out.println("Introduzca una opciÃ³n vÃ¡lida.");
 			}
 		}
 	System.out.println("Programa finalizado.");
@@ -47,7 +52,6 @@ public class practicatema6 {
 	
 	
 	public static int mayor (int[] array) {
-		int max = 0;
 		int i = 0;
 		for (i=0; i<20; i++) {
 			if (array[i] > max) {
@@ -58,7 +62,6 @@ public class practicatema6 {
 	}
 	
 	public static int menor (int[] array) {
-		int min = 1;
 		int i = 0;
 		for (i=0; i<20; i++) {
 			if (array[i] < min) {
@@ -69,7 +72,6 @@ public class practicatema6 {
 	}
 	
 	public static double media (int[] array) {
-		double avg = 0;
 		int i = 0;
 		for (i=0; i<20; i++) {
 			avg = avg + array[i];	
@@ -88,7 +90,6 @@ public class practicatema6 {
 	}
 	
 	public static int buscar (int[] array, int numero) {
-		int veces = 0;
 		int i = 0;
 		for (i=0; i<20; i++) {
 			if (numero == array[i]) {
